8b5d1cfe28d3c6b2bc1d648f54d6eb493bdb58e6
Abril Navarrete <itziar.abril.navarrete.vazquez@gmail.com>
2026-06-18 12:51:26 -0600

    Clase 8. Implementacion de Factorial iterativo vs recursivo y resolucion de desbordamiento mediante BigInteger

---
+++ EB8_FActorialOverflow/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Numerics; 
+
+namespace FactorialApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            //+++++++++++++++++++  PARTE A  IMPLMENTACION TRADCIONAL Y EL PUNTO DE QUIEBRE+++++++++++++++++++++++++++
+            for (int i = 1; i <= 20; i++)
+            {
+                Console.WriteLine($"n={i:D2} | Resultado (int): {FactorialInt(i),12}");
+            }
+            Console.WriteLine("\n[Documentación]: A partir de n=13, el valor excede el límite del 'int' (32 bits), causando desbordamiento.");
+
+
+
+
+
+
+            // +++++++++++++++++ PARTE B EL  BIGINTEGRE +++++++++++++++++++
+            BigInteger n = 100;
+            Console.WriteLine($"Factorial de {n} es:");
+            Console.WriteLine(FactorialProfesional(n));
+        }
+        static int FactorialInt(int n)
+        {
+            if (n == 0 || n == 1) return 1;
+            return n * FactorialInt(n - 1);
+        }
+        static BigInteger FactorialProfesional(BigInteger n)
+        {
+            if (n == 0 || n == 1) return BigInteger.One;
+            return n * FactorialProfesional(n - 1);
+        }
+    }
+}
